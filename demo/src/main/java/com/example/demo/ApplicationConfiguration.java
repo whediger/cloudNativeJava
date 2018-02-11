9562ca0d50cc657999c9520d875053035b60c4f5
Wes Hediger <wes.hediger@gmail.com>
2018-02-11 17:45:20 -0600

    add CustomerRestController

--- demo/src/main/java/com/example/demo/ApplicationConfiguration.java
+++ demo/src/main/java/com/example/demo/ApplicationConfiguration.java
@@ -1,25 +1,8 @@
-package com.example.psa;
+package com.example.boot;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
-
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class ApplicationConfiguration {
 	
-	@Bean(destroyMethod = "shutdown")
-	DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-				.setName("customers").build();
-	}
-	
-	@Bean
-	JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
 }
