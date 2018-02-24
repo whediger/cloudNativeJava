a7c00d00777921940bf51fa97b33426c5d804399
Wes Hediger <wes.hediger@gmail.com>
2018-02-23 19:39:10 -0600

    add LoggingAroundAspect.java

---
+++ demo/src/main/java/com/example/aop/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.aop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbaeddedDatabeseType;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class ApplicationConfiguration{
+
+    @Bean(destroyMethod = "shutDown")
+    Datasource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+            .setName("customers").build();
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
